ef17c47e72ea7e30ec786847d270c1fc02c6caac
Adrián Villa <idgs.adrian.villa@gmail.com>
2024-03-02 13:34:15 -0600

    Se ajsutan flujos principales

--- src/flow/start/flowPrincipal.js
+++ src/flow/start/flowPrincipal.js
@@ -1,18 +1,24 @@
 const { addKeyword } = require('@bot-whatsapp/bot');
 
+const { flowConsultaPlanes } = require('../web/flowConsultaPlanes');
+const { flowCoberturaInternet } = require('../web/flowCoberturaInternet');
+const { flowContratacion } = require('../web/flowContratacion');
+const { flowPagoInternet } = require('../web/flowPagoInternet');
+
 const { obtenerSaludo } = require('../../services/web.service');
 const { obtenerOpcionesFlujoPrincipal } = require('../../services/generic.service');
 
 const flowPrincipal = addKeyword(['hola', 'menu'])
-    .addAction(
-        async (_, { flowDynamic }) => {
-            const mensaje = `🤖 ¿En que puedo ayudarte el día de hoy?\n${await obtenerOpcionesFlujoPrincipal()}`
-
-            return await flowDynamic([
-                `🙌 Hola ${obtenerSaludo()}, bienvenido al chatbot de *Emenet*`,
-                mensaje+'\n\n Realice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team'
-            ]);
-        }
+    .addAnswer(`🙌 Hola ${obtenerSaludo()}, bienvenido al chatbot de *Emenet*`)
+    .addAnswer(
+        [
+            `🤖 ¿En que puedo ayudarte el día de hoy?\n${obtenerOpcionesFlujoPrincipal()}`,
+            '',
+            'Realice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team'
+        ],
+        null,
+        null,
+        [flowConsultaPlanes, flowCoberturaInternet, flowContratacion, flowPagoInternet]
     )
 
 module.exports = { flowPrincipal };
--- src/flow/start/flowSecundario.js
+++ src/flow/start/flowSecundario.js
@@ -1,13 +1,22 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
 
+const { flowConsultaPlanes } = require('../web/flowConsultaPlanes');
+const { flowCoberturaInternet } = require('../web/flowCoberturaInternet');
+const { flowContratacion } = require('../web/flowContratacion');
+const { flowPagoInternet } = require('../web/flowPagoInternet');
+
 const { obtenerOpcionesFlujoPrincipal } = require("../../services/generic.service");
 
 const flowSecundario = addKeyword(['no'], { sensitive: true })
-    .addAction(
-        async (_, { flowDynamic }) => {
-            const mensaje = `🤖 ¿Algo más en lo que pueda ayudarte el día de hoy?\n${await obtenerOpcionesFlujoPrincipal()}`
-            return await flowDynamic(mensaje + '\n\nRealice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team');
-        }
+    .addAnswer(
+        [
+            `🤖 ¿Algo más en lo que pueda ayudarte el día de hoy?\n${obtenerOpcionesFlujoPrincipal()}`,
+            '',
+            'Realice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team'
+        ],
+        null,
+        null,
+        [flowConsultaPlanes, flowCoberturaInternet, flowContratacion, flowPagoInternet]
     )
 
 module.exports = { flowSecundario };
