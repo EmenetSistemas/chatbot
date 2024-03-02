95a933205e3183d5332a096d4caba54889fe8a94
Adrián Villa <idgs.adrian.villa@gmail.com>
2024-03-02 12:37:38 -0600

    Se ajusta flujo consulta planes

--- src/flow/start/flowSecundario.js
+++ src/flow/start/flowSecundario.js
@@ -1,15 +1,12 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
 
-const { crearMensajeConBotones, obtenerOpcionesFlujoPrincipal } = require("../../services/generic.service");
+const { obtenerOpcionesFlujoPrincipal } = require("../../services/generic.service");
 
 const flowSecundario = addKeyword(['no'], { sensitive: true })
     .addAction(
         async (_, { flowDynamic }) => {
             const mensaje = `🤖 ¿Algo más en lo que pueda ayudarte el día de hoy?\n${await obtenerOpcionesFlujoPrincipal()}`
-            return await flowDynamic([
-                `🙌 Hola ${obtenerSaludo()}, bienvenido al chatbot de *Emenet*`,
-                mensaje + '\n\nRealice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team'
-            ]);
+            return await flowDynamic(mensaje + '\n\nRealice un test de velicidad en el siguiente link: https://emenet.m-net.mx/inicio#team');
         }
     )
 
