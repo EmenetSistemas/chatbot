e1eebf832801e4cb484e674f7b40eba6a9829940
Adrián Villa <idgs.adrian.villa@gmail.com>
2024-03-21 11:00:18 -0600

    Correción deploy api

--- app/src/app.js
+++ app/src/app.js
@@ -11,17 +11,24 @@ const router = require('../routes/api');
 const main = async () => {
     const app = express();
     app.use('/api', router);
-    
+
     await createBot({
         flow,
         provider,
         database: new MockAdapter()
     }, {
-        blackList:[]
+        blackList: []
     });
-    
-    QRPortalWeb();
-    const PORT = 4000;
+
+    app.get("/get-qr", async (_, res) => {
+        const YOUR_PATH_QR = join(process.cwd(), `bot.qr.png`);
+        const fileStream = createReadStream(YOUR_PATH_QR);
+
+        res.writeHead(200, { "Content-Type": "image/png" });
+        fileStream.pipe(res);
+    });
+
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 }
 
